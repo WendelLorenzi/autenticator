7b7a2e313004dd1a63b0c6366c35e73e0db03ef4
WendelLorenzi <wlorenzi17@gmail.com>
2023-05-22 17:32:18 -0300

    feat: first structure

---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+const router = Router()
+
+router.post('/users', (request, response) => {
+  return createUserController.handle(request, response);
+});
+
+export { router }
