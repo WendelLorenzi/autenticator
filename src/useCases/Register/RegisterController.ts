863298e19c8c9c6bb24ae55d7bd8621902dae52f
WendelLorenzi <wlorenzi17@gmail.com>
2023-05-28 00:12:17 -0300

    feat: adjusst buidspec and private route register

--- src/useCases/Register/RegisterController.ts
+++ src/useCases/Register/RegisterController.ts
@@ -10,10 +10,10 @@ export class RegisterController {
 
     async handle(request: Request, response: Response): Promise<Response> {
         const { name, email, password } = request.body;
-        // const auth = await this.authmiddleware.handle(request);
+        const auth = await this.authmiddleware.handle(request);
 
         try {
-            // if (auth) {
+            if (auth) {
                 const token = await this.registerUseCase.execute({
                     name,
                     email,
@@ -21,7 +21,7 @@ export class RegisterController {
                 });
                 if (token) return response.status(200).header("x-acess-token", token).send();
                 return response.status(400).end();
-            // }
+            }
             return response.status(401).end();
         }
         catch {
