437371bc65b201651f1d7dedd99d07ed065d8cc7
WendelLorenzi <wlorenzi17@gmail.com>
2023-06-12 16:41:03 -0300

    feat: add bodyParser

--- src/app.ts
+++ src/app.ts
@@ -5,17 +5,18 @@ import connectDB from './providers/mongoDB/connection';
 import swagguerUi from 'swagger-ui-express';
 import swaggerDocs from './swagger.json';
 import Cors from './useCases/middleware/Cors';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded());
+
+app.use(bodyParser.json());                        
+app.use(bodyParser.urlencoded({ extended: true }));
+
 connectDB();
 app.use('/docs', swagguerUi.serve, swagguerUi.setup(swaggerDocs));
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', process.env.APP);
-    next();
-});
 app.use(Cors);
 app.use(router);
 
